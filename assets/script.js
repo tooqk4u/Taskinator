ee7923b85c934585e6db887ea052c42dd5872e29
tooqk4u <tooqk4u@protonmail.com>
2022-01-03 14:35:34 -0500

    created button handler and function click initiates

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+let buttonEl = document.querySelector("#save-task");
+let tasksToDoEl = document.querySelector("#tasks-to-do");
+
+let createTaskHandler = function(){
+  let listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
 
 
 
 
-buttonEl.addEventListener("click", function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  });
+
+
